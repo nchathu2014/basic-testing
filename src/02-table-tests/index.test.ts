d67286187f603f2aeefd12b66f3067a735a20d65
Nuwan Thuduwage <nuwan.thuduwage@autozone.com>
2025-05-04 01:42:42 -0400

    fix: completed test cases

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,77 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+
+  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
+  { a: -3, b: -2, action: Action.Subtract, expected: -1 },
+  { a: 4, b: 12, action: Action.Subtract, expected: -8 },
+
+  { a: 3, b: 0, action: Action.Multiply, expected: 0 },
+  { a: -3, b: -5, action: Action.Multiply, expected: 15 },
+  { a: -6, b: -5, action: Action.Multiply, expected: 30 },
+
+  { a: 6, b: 3, action: Action.Divide, expected: 2 },
+  { a: -6, b: -3, action: Action.Divide, expected: 2 },
+  { a: -6, b: 3, action: Action.Divide, expected: -2 },
+
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: -2, b: -3, action: Action.Exponentiate, expected: -0.125 },
+  { a: -2, b: 3, action: Action.Exponentiate, expected: -8 },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test('should perform addition correctly', () => {
+    const additionCases = testCases.filter((tc) => tc.action === Action.Add);
+    additionCases.forEach(({ a, b, action, expected }) => {
+      const input = { a, b, action };
+      const result = simpleCalculator(input);
+      expect(result).toBe(expected);
+    });
   });
-  // Consider to use Jest table tests API to test all cases above
+
+  test('should perform subtraction correctly', () => {
+    const subtractionCases = testCases.filter(
+      (tc) => tc.action === Action.Subtract,
+    );
+    subtractionCases.forEach(({ a, b, action, expected }) => {
+      const input = { a, b, action };
+      const result = simpleCalculator(input);
+      expect(result).toBe(expected);
+    });
+  });
+
+  test('should perform multiplication correctly', () => {
+    const multiplicationCases = testCases.filter(
+      (tc) => tc.action === Action.Multiply,
+    );
+    multiplicationCases.forEach(({ a, b, action, expected }) => {
+      const input = { a, b, action };
+      const result = simpleCalculator(input);
+      expect(result).toBe(expected);
+    });
+  });
+
+  test('should perform division correctly', () => {
+    const divisionCases = testCases.filter((tc) => tc.action === Action.Divide);
+    divisionCases.forEach(({ a, b, action, expected }) => {
+      const input = { a, b, action };
+      const result = simpleCalculator(input);
+      expect(result).toBe(expected);
+    });
+  });
+
+  test('should perform exponentiation correctly', () => {
+    const exponentiationCases = testCases.filter(
+      (tc) => tc.action === Action.Exponentiate,
+    );
+    exponentiationCases.forEach(({ a, b, action, expected }) => {
+      const input = { a, b, action };
+      const result = simpleCalculator(input);
+      expect(result).toBe(expected);
+    });
+  });
 });
--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,44 +1,89 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { getBankAccount } from '.';
 
 describe('BankAccount', () => {
   test('should create account with initial balance', () => {
-    // Write your test here
+    const initialBalance = 100;
+    const account = getBankAccount(initialBalance);
+    expect(account.getBalance()).toBe(initialBalance);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    const balance = 100;
+    const account = getBankAccount(balance);
+    const withdrawAmount = 200;
+    expect(() => account.withdraw(withdrawAmount)).toThrow(
+      `Insufficient funds: cannot withdraw more than ${balance}`,
+    );
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    const balance = 100;
+    const account = getBankAccount(balance);
+    const transferAmount = 200;
+    const toAccount = getBankAccount(0);
+    expect(() => account.transfer(transferAmount, toAccount)).toThrow(
+      `Insufficient funds: cannot withdraw more than ${balance}`,
+    );
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    const balance = 100;
+    const account = getBankAccount(balance);
+    expect(() => account.transfer(50, account)).toThrow('Transfer failed');
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const initialBalance = 100;
+    const depositAmount = 50;
+    const account = getBankAccount(initialBalance);
+    account.deposit(depositAmount);
+    expect(account.getBalance()).toBe(initialBalance + depositAmount);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const initialBalance = 100;
+    const account = getBankAccount(initialBalance);
+    const withdrawAmount = 50;
+    account.withdraw(withdrawAmount);
+    expect(account.getBalance()).toBe(initialBalance - withdrawAmount);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const initialBalance = 100;
+    const transferAmount = 50;
+    const account = getBankAccount(initialBalance);
+    const toAccount = getBankAccount(0);
+    account.transfer(transferAmount, toAccount);
+    expect(account.getBalance()).toBe(initialBalance - transferAmount);
+    expect(toAccount.getBalance()).toBe(transferAmount);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    const initialBalance = 100;
+    const account = getBankAccount(initialBalance);
+    const fetchBalanceSpy = jest.spyOn(account, 'fetchBalance');
+    fetchBalanceSpy.mockResolvedValue(50);
+    const balance = await account.fetchBalance();
+    expect(fetchBalanceSpy).toHaveBeenCalled();
+    expect(balance).toBe(50);
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
+    const initialBalance = 100;
+    const account = getBankAccount(initialBalance);
+    const newBalance = 50;
+    jest.spyOn(account, 'fetchBalance').mockResolvedValue(newBalance);
+    await account.synchronizeBalance();
+    expect(account.getBalance()).toBe(newBalance);
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    // Write your tests here
+    const initialBalance = 100;
+    const account = getBankAccount(initialBalance);
+    jest.spyOn(account, 'fetchBalance').mockResolvedValue(null);
+    await expect(account.synchronizeBalance()).rejects.toThrow(
+      'Synchronization failed',
+    );
   });
 });
